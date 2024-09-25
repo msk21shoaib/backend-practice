7a70d99dc55f63051fb83b9908509de2ab0724ca
Muhammad Shoaib Khan <144353922+msk21shoaib@users.noreply.github.com>
2024-09-25 09:56:13 +0530

    utils section

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+//this is just a bundler code that will help further as the code will be used more than once
+
+/*
+const asyncHandler = () => {}   ------- usual
+const asyncHandler = (func) => { async () => {} }  
+*/
+
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+/*
+const asyncHandler = (func) => async (req,res,next) => {
+    try{
+
+    } catch(error) {
+        res.status(err.code || 500).json({
+            success:false,
+            message: err.message
+        })
+    }
+}
+*/
